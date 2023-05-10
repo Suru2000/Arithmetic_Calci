7ade349c87d741c0b68207176b1c389781fb7eef
Suru2000 <86404115+Suru2000@users.noreply.github.com>
2023-05-10 09:37:52 +0530

    Arithmetic_calci

---
+++ Arithmetic_Calci/src/arithmetic_calci/Arithmetic_calci.java
@@ -0,0 +1,10 @@
+package arithmetic_calci;
+
+public class Arithmetic_calci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Arithmetic_Calci/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Adminastrator
+ *
+ */
+module Arithmetic_Calci {
+}
