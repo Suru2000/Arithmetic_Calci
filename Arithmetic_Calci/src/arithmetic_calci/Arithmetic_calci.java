59183e866797314b918a6e58f390782f848e6520
Suru2000 <86404115+Suru2000@users.noreply.github.com>
2023-05-10 10:21:22 +0530

    Arithmetic_calci.java

--- Arithmetic_Calci/src/arithmetic_calci/Arithmetic_calci.java
+++ Arithmetic_Calci/src/arithmetic_calci/Arithmetic_calci.java
@@ -1,10 +1,38 @@
 package arithmetic_calci;
-
+import java.util.Scanner;
 public class Arithmetic_calci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		  char operator;
+		  double number1, number2, result;
+		  Scanner sc = new Scanner(System.in);
+		  System.out.println("Choose an operator: +, -, *, or /");
+		  operator = sc.next().charAt(0);
+		  System.out.println("Enter first number");
+		  number1 = sc.nextDouble();
+		  System.out.println("Enter second number");
+		  number2 = sc.nextDouble();
+		  switch (operator) {
+		  case '+':
+			  result = number1 + number2;
+			  System.out.println(number1 + " + " + number2 + " = " + result);
+		        break;
+		        case '-':
+		        	result = number1 - number2;
+		        	System.out.println(number1 + " - " + number2 + " = " + result);
+		        	break;
+		        	case '*':
+		        		result = number1 * number2;
+		        		System.out.println(number1 + " * " + number2 + " = " + result);
+		        		break;
+		        		case '/':
+		        			result = number1 / number2;
+		        			System.out.println(number1 + " / " + number2 + " = " + result);
+		        			break;
+		        			default:
+		        				System.out.println("Invalid operator!");
+		        				break;
+		        				}
+		  sc.close();
+		  }
 	}
-
-}
